3c76cec27ff56b581d00593707998fdbfc2c4b6e
STUPID Modder <stupidmoddersoup@gmail.com>
2023-12-18 00:42:39 -0300

    implement ThreadUtil::PlaceMemoryBarrier

--- sead/include/thread/seadThreadUtil.h
+++ sead/include/thread/seadThreadUtil.h
@@ -2,6 +2,12 @@
 
 #include <basis/seadTypes.h>
 
+#ifdef SEAD_PLATFORM_WINDOWS
+#define NOMINMAX
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#endif // SEAD_PLATFORM_WINDOWS
+
 namespace sead {
 
 // TODO
@@ -11,7 +17,15 @@ public:
     static s32 ConvertPrioritySeadToPlatform(s32 prio);
     static s32 ConvertPriorityPlatformToSead(s32 prio);
     static uintptr_t GetCurrentStackPointer();
-    static void PlaceMemoryBarrier();
+
+    static void PlaceMemoryBarrier()
+    {
+#ifdef SEAD_PLATFORM_WINDOWS
+        MemoryBarrier();
+#else
+#error "Unsupported platform"
+#endif // SEAD_PLATFORM_WINDOWS
+    }
 };
 
 } // namespace sead
