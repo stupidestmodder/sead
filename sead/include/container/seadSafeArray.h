2c282047d85ffaf88ff45e7663556a51bf2002b3
STUPID Modder <stupidmoddersoup@gmail.com>
2024-01-06 18:30:56 -0300

    container: Add SafeArray (TODO)

---
+++ sead/include/container/seadSafeArray.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <basis/seadAssert.h>
+#include <basis/seadTypes.h>
+
+namespace sead {
+
+template <typename T, s32 N>
+class SafeArray;
+
+template <typename T>
+class SafeArray<T, 0>
+{
+};
+
+// TODO
+template <typename T, s32 N>
+class SafeArray
+{
+public:
+    SafeArray()
+        : mBuffer()
+    {
+    }
+
+    T& operator[](s32 x)
+    {
+        if (static_cast<u32>(x) >= static_cast<u32>(N))
+        {
+            SEAD_ASSERT_MSG(false, "range over [0, %d) : %d", N, x);
+            return mBuffer[0];
+        }
+
+        return mBuffer[x];
+    }
+
+    const T& operator[](s32 x) const
+    {
+        if (static_cast<u32>(x) >= static_cast<u32>(N))
+        {
+            SEAD_ASSERT_MSG(false, "range over [0, %d) : %d", N, x);
+            return mBuffer[0];
+        }
+
+        return mBuffer[x];
+    }
+
+private:
+    T mBuffer[N];
+};
+
+} // namespace sead
