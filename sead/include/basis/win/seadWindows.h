054843144eb84474ba3ad6f3a4369640adf22fc5
STUPID Modder <stupidmoddersoup@gmail.com>
2024-01-27 19:17:40 -0300

    basis: Add windows include header

---
+++ sead/include/basis/win/seadWindows.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <basis/seadTypes.h>
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif // WIN32_LEAN_AND_MEAN
+
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif // NOMINMAX
+
+#include <windows.h>
