0f80c39b3aeaee43649737a7994c905360946d3f
STUPID Modder <stupidmoddersoup@gmail.com>
2024-06-08 21:52:03 -0300

    geom: Add Line header

--- sead/include/geom/seadLine.h
+++ sead/include/geom/seadLine.h
@@ -9,12 +9,19 @@ template <typename T>
 class Ray
 {
 public:
+    Ray();
+
+    void setBy2Points(const T& p0, const T& p1);
+
+    void setPos(const T& p);
+    void setDir(const T& d, bool isNormalized);
 
 private:
     T mP;
     T mD;
 };
 
+using Ray2f = Ray<Vector2f>;
 using Ray3f = Ray<Vector3f>;
 
 } // namespace sead
