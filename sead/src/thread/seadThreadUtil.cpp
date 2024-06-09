f9c7db010581abb5301212df8ab651c4c501e4e1
STUPID Modder <stupidmoddersoup@gmail.com>
2024-06-08 21:51:22 -0300

    thread: Implement TODO function

--- sead/src/thread/seadThreadUtil.cpp
+++ sead/src/thread/seadThreadUtil.cpp
@@ -1,7 +1,16 @@
 #include <thread/seadThreadUtil.h>
 
+#include <basis/seadAssert.h>
+
 namespace sead {
 
-
+// TODO
+s32 ThreadUtil::ConvertPrioritySeadToPlatform(s32 prio)
+{
+    SEAD_ASSERT(prio >= 0);
+    SEAD_ASSERT(prio < 32);
+    //return prio;
+    return THREAD_PRIORITY_TIME_CRITICAL;
+}
 
 } // namespace sead
