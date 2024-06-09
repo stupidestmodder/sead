986d9631dcfe232eae6755bd7c96b7cd66c8db55
STUPID Modder <stupidmoddersoup@gmail.com>
2024-06-08 21:50:50 -0300

    resource: Add todo ResourceMgr header

--- sead/include/resource/seadResourceMgr.h
+++ sead/include/resource/seadResourceMgr.h
@@ -1,15 +1,31 @@
 #pragma once
 
+#include <container/seadTList.h>
 #include <heap/seadDisposer.h>
 
 namespace sead {
+
+class Decompressor;
+class ResourceFactory;
 
 // TODO
 class ResourceMgr
 {
     SEAD_SINGLETON_DISPOSER(ResourceMgr);
 
+private:
+    using FactoryList = TList<ResourceFactory*>;
+    using DecompressorList = TList<Decompressor*>;
+
 public:
+    ResourceMgr();
+    ~ResourceMgr();
+
+protected:
+    FactoryList mFactoryList;
+    DecompressorList mDecompList;
+    ResourceFactory* mNullResourceFactory;
+    ResourceFactory* mDefaultResourceFactory;
 };
 
 } // namespace sead
