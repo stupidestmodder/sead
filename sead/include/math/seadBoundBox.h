94c015b6aafa7812568e5836d83ae0e92bde9ecc
STUPID Modder <stupidmoddersoup@gmail.com>
2026-02-27 03:52:17 -0300

    math: FInish BoundBox2/3

--- sead/include/math/seadBoundBox.h
+++ sead/include/math/seadBoundBox.h
@@ -5,12 +5,11 @@
 
 namespace sead {
 
-// TODO
 template <typename T>
 class BoundBox2
 {
 public:
-    using Vector2 = sead::Vector2<T>;
+    using Vector2 = Vector2<T>;
 
 public:
     BoundBox2()
@@ -23,35 +22,29 @@ public:
         set(x0, y0, x1, y1);
     }
 
-    void set(T x0, T y0, T x1, T y1)
+    BoundBox2(const Vector2& min, const Vector2& max)
     {
-        if (x0 < x1)
-        {
-            mMin.x = x0;
-            mMax.x = x1;
-        }
-        else
-        {
-            mMin.x = x1;
-            mMax.x = x0;
-        }
-
-        if (y0 < y1)
-        {
-            mMin.y = y0;
-            mMax.y = y1;
-        }
-        else
-        {
-            mMin.y = y1;
-            mMax.y = y0;
-        }
+        set(min, max);
     }
 
-    void setUndef()
+    bool isUndef() const;
+
+    void set(T x0, T y0, T x1, T y1);
+    void set(const Vector2& min, const Vector2& max);
+    void setUndef();
+    void setFromCornerAndXY(T cornerX, T cornerY, T sizeX, T sizeY);
+    void setFromCornerAndXY(const Vector2& corner, T sizeX, T sizeY);
+    void setFromCenterAndXY(T centerX, T centerY, T sizeX, T sizeY);
+    void setFromCenterAndXY(const Vector2& center, T sizeX, T sizeY); //? This one is implemeted here but let's not.
+
+    void setMin(const Vector2& min)
     {
-        mMin.set(MathCalcCommon<T>::maxNumber(), MathCalcCommon<T>::maxNumber());
-        mMax.set(MathCalcCommon<T>::minNumber(), MathCalcCommon<T>::minNumber());
+        mMin = min;
+    }
+
+    void setMax(const Vector2& max)
+    {
+        mMax = max;
     }
 
     T getSizeX() const
@@ -66,17 +59,12 @@ public:
 
     T getHalfSizeX() const
     {
-        return (mMax.x - mMin.x) / 2.0f;
+        return getSizeX() / 2.0f;
     }
 
     T getHalfSizeY() const
     {
-        return (mMax.y - mMin.y) / 2.0f;
-    }
-
-    Vector2& getMin()
-    {
-        return mMin;
+        return getSizeY() / 2.0f;
     }
 
     const Vector2& getMin() const
@@ -84,9 +72,9 @@ public:
         return mMin;
     }
 
-    Vector2& getMax()
+    Vector2& getMin()
     {
-        return mMax;
+        return mMin;
     }
 
     const Vector2& getMax() const
@@ -94,38 +82,164 @@ public:
         return mMax;
     }
 
-    void setMin(const Vector2& min)
+    Vector2& getMax()
     {
-        mMin = min;
+        return mMax;
     }
 
-    void setMax(const Vector2& max)
+    Vector2 getTL() const
     {
-        mMax = max;
+        return mMin;
+    }
+
+    Vector2 getTR() const
+    {
+        return Vector2(mMax.x, mMin.y);
+    }
+
+    Vector2 getBL() const
+    {
+        return Vector2(mMin.x, mMax.y);
+    }
+
+    Vector2 getBR() const
+    {
+        return mMax;
     }
 
     Vector2 getCenter() const;
+    void getCenter(Vector2* p) const;
 
-    void getCenter(Vector2* p) const
-    {
-        p->set((mMin.x + mMax.x) / 2.0f, (mMin.y + mMax.y) / 2.0f);
-    }
+    bool isInside(const Vector2& p) const;
 
-    bool isUndef() const
-    {
-        return mMin.x > mMax.x || mMin.y > mMax.y;
-    }
-
-    bool isInside(const Vector2& p) const
-    {
-        return mMin.x <= p.x && p.x <= mMax.x && mMin.y <= p.y && p.y <= mMax.y;
-    }
+    void offset(T dx, T dy);
+    void offset(const Vector2& dv);
+    void scaleX(T sx);
+    void scaleY(T sy);
 
 private:
     Vector2 mMin;
     Vector2 mMax;
 };
 
+template <typename T>
+class BoundBox3
+{
+public:
+    using Vector3 = Vector3<T>;
+
+public:
+    BoundBox3()
+    {
+        setUndef();
+    }
+
+    BoundBox3(T x0, T y0, T z0, T x1, T y1, T z1)
+    {
+        set(x0, y0, z0, x1, y1, z1);
+    }
+
+    BoundBox3(const Vector3& min, const Vector3& max)
+    {
+        set(min, max);
+    }
+
+    bool isUndef() const;
+
+    void set(T x0, T y0, T z0, T x1, T y1, T z1);
+    void set(const Vector3& min, const Vector3& max);
+    void setUndef();
+    void setFromCornerAndXYZ(T cornerX, T cornerY, T cornerZ, T sizeX, T sizeY, T sizeZ);
+    void setFromCornerAndXYZ(const Vector3& corner, T sizeX, T sizeY, T sizeZ);
+    void setFromCenterAndXYZ(T centerX, T centerY, T centerZ, T sizeX, T sizeY, T sizeZ);
+    void setFromCenterAndXYZ(const Vector3& center, T sizeX, T sizeY, T sizeZ);
+
+    void setMin(const Vector3& min)
+    {
+        mMin = min;
+    }
+
+    void setMax(const Vector3& max)
+    {
+        mMax = max;
+    }
+
+    T getSizeX() const
+    {
+        return mMax.x - mMin.x;
+    }
+
+    T getSizeY() const
+    {
+        return mMax.y - mMin.y;
+    }
+
+    T getSizeZ() const
+    {
+        return mMax.z - mMin.z;
+    }
+
+    T getHalfSizeX() const
+    {
+        return getSizeX() / 2.0f;
+    }
+
+    T getHalfSizeY() const
+    {
+        return getSizeY() / 2.0f;
+    }
+
+    T getHalfSizeZ() const
+    {
+        return getSizeZ() / 2.0f;
+    }
+
+    const Vector3& getMin() const
+    {
+        return mMin;
+    }
+
+    Vector3& getMin()
+    {
+        return mMin;
+    }
+
+    const Vector3& getMax() const
+    {
+        return mMax;
+    }
+
+    Vector3& getMax()
+    {
+        return mMax;
+    }
+
+    Vector3 getCenter() const;
+    void getCenter(Vector3* p) const;
+
+    bool isInside(const Vector3& p) const;
+
+    void offset(T dx, T dy, T dz);
+    void offset(const Vector3& dv);
+    void scaleX(T sx);
+    void scaleY(T sy);
+    void scaleZ(T sz);
+
+private:
+    Vector3 mMin;
+    Vector3 mMax;
+};
+
 using BoundBox2f = BoundBox2<f32>;
+using BoundBox2d = BoundBox2<f64>;
+using BoundBox2i = BoundBox2<s32>;
+using BoundBox2u = BoundBox2<u32>;
+
+using BoundBox3f = BoundBox3<f32>;
+using BoundBox3d = BoundBox3<f64>;
+using BoundBox3i = BoundBox3<s32>;
+using BoundBox3u = BoundBox3<u32>;
 
 } // namespace sead
+
+#include <math/seadBoundBox.hpp>
--- sead/include/math/seadBoundBox.hpp
+++ sead/include/math/seadBoundBox.hpp
@@ -1,7 +1,290 @@
 #pragma once
 
+#include <math/seadBoundBox.h>
+
 namespace sead {
 
+template <typename T>
+inline bool BoundBox2<T>::isUndef() const
+{
+    return mMin.x > mMax.x || mMin.y > mMax.y;
+}
 
+template <typename T>
+inline void BoundBox2<T>::set(T x0, T y0, T x1, T y1)
+{
+    if (x0 < x1)
+    {
+        mMin.x = x0;
+        mMax.x = x1;
+    }
+    else
+    {
+        mMin.x = x1;
+        mMax.x = x0;
+    }
+
+    if (y0 < y1)
+    {
+        mMin.y = y0;
+        mMax.y = y1;
+    }
+    else
+    {
+        mMin.y = y1;
+        mMax.y = y0;
+    }
+}
+
+template <typename T>
+inline void BoundBox2<T>::set(const Vector2& min, const Vector2& max)
+{
+    mMin = min;
+    mMax = max;
+}
+
+template <typename T>
+inline void BoundBox2<T>::setUndef()
+{
+    mMin.set(MathCalcCommon<T>::maxNumber(), MathCalcCommon<T>::maxNumber());
+    mMax.set(MathCalcCommon<T>::minNumber(), MathCalcCommon<T>::minNumber());
+}
+
+template <typename T>
+inline void BoundBox2<T>::setFromCornerAndXY(T cornerX, T cornerY, T sizeX, T sizeY)
+{
+    mMin.set(cornerX, cornerY);
+    mMax.set(cornerX + sizeX, cornerY + sizeY);
+}
+
+template <typename T>
+inline void BoundBox2<T>::setFromCornerAndXY(const Vector2& corner, T sizeX, T sizeY)
+{
+    setFromCornerAndXY(corner.x, corner.y, sizeX, sizeY);
+}
+
+template <typename T>
+inline void BoundBox2<T>::setFromCenterAndXY(T centerX, T centerY, T sizeX, T sizeY)
+{
+    T halfWidth = sizeX / 2.0f;
+    T halfHeight = sizeY / 2.0f;
+
+    mMin.set(centerX - halfWidth, centerY - halfHeight);
+    mMax.set(centerX + halfWidth, centerY + halfHeight);
+}
+
+template <typename T>
+inline void BoundBox2<T>::setFromCenterAndXY(const Vector2& center, T sizeX, T sizeY)
+{
+    setFromCenterAndXY(center.x, center.y, sizeX, sizeY);
+}
+
+template <typename T>
+Vector2<T> BoundBox2<T>::getCenter() const
+{
+    return Vector2((mMin.x + mMax.x) / 2.0f, (mMin.y + mMax.y) / 2.0f);
+}
+
+template <typename T>
+void BoundBox2<T>::getCenter(Vector2* p) const
+{
+    p->set((mMin.x + mMax.x) / 2.0f, (mMin.y + mMax.y) / 2.0f);
+}
+
+template <typename T>
+bool BoundBox2<T>::isInside(const Vector2& p) const
+{
+    return mMin.x <= p.x && p.x <= mMax.x &&
+           mMin.y <= p.y && p.y <= mMax.y;
+}
+
+template <typename T>
+inline void BoundBox2<T>::offset(T dx, T dy)
+{
+    mMin.x += dx;
+    mMin.y += dy;
+    mMax.x += dx;
+    mMax.y += dy;
+}
+
+template <typename T>
+inline void BoundBox2<T>::offset(const Vector2& dv)
+{
+    offset(dv.x, dv.y);
+}
+
+template <typename T>
+inline void BoundBox2<T>::scaleX(T sx)
+{
+    T hx = getSizeX() * (sx / 2.0f);
+    T ox = (mMin.x + mMax.x) / 2.0f;
+
+    mMin.x = ox - hx;
+    mMax.x = ox + hx;
+}
+
+template <typename T>
+inline void BoundBox2<T>::scaleY(T sy)
+{
+    T hy = getSizeY() * (sy / 2.0f);
+    T oy = (mMin.y + mMax.y) / 2.0f;
+
+    mMin.y = oy - hy;
+    mMax.y = oy + hy;
+}
+
+template <typename T>
+inline bool BoundBox3<T>::isUndef() const
+{
+    return mMin.x > mMax.x || mMin.y > mMax.y || mMin.z > mMax.z;
+}
+
+template <typename T>
+inline void BoundBox3<T>::set(T x0, T y0, T z0, T x1, T y1, T z1)
+{
+    if (x0 < x1)
+    {
+        mMin.x = x0;
+        mMax.x = x1;
+    }
+    else
+    {
+        mMin.x = x1;
+        mMax.x = x0;
+    }
+
+    if (y0 < y1)
+    {
+        mMin.y = y0;
+        mMax.y = y1;
+    }
+    else
+    {
+        mMin.y = y1;
+        mMax.y = y0;
+    }
+
+    if (z0 < z1)
+    {
+        mMin.z = z0;
+        mMax.z = z1;
+    }
+    else
+    {
+        mMin.z = z1;
+        mMax.z = z0;
+    }
+}
+
+template <typename T>
+inline void BoundBox3<T>::set(const Vector3& min, const Vector3& max)
+{
+    mMin = min;
+    mMax = max;
+}
+
+template <typename T>
+inline void BoundBox3<T>::setUndef()
+{
+    mMin.set(MathCalcCommon<T>::maxNumber(), MathCalcCommon<T>::maxNumber(), MathCalcCommon<T>::maxNumber());
+    mMax.set(MathCalcCommon<T>::minNumber(), MathCalcCommon<T>::minNumber(), MathCalcCommon<T>::minNumber());
+}
+
+template <typename T>
+inline void BoundBox3<T>::setFromCornerAndXYZ(T cornerX, T cornerY, T cornerZ, T sizeX, T sizeY, T sizeZ)
+{
+    mMin.set(cornerX, cornerY, cornerZ);
+    mMax.set(cornerX + sizeX, cornerY + sizeY, cornerZ + sizeZ);
+}
+
+template <typename T>
+inline void BoundBox3<T>::setFromCornerAndXYZ(const Vector3& corner, T sizeX, T sizeY, T sizeZ)
+{
+    setFromCornerAndXYZ(corner.x, corner.y, corner.z, sizeX, sizeY, sizeZ);
+}
+
+template <typename T>
+inline void BoundBox3<T>::setFromCenterAndXYZ(T centerX, T centerY, T centerZ, T sizeX, T sizeY, T sizeZ)
+{
+    T halfWidth = sizeX / 2.0f;
+    T halfHeight = sizeY / 2.0f;
+    T halfDepth = sizeZ / 2.0f;
+
+    mMin.set(centerX - halfWidth, centerY - halfHeight, centerZ - halfDepth);
+    mMax.set(centerX + halfWidth, centerY + halfHeight, centerZ + halfDepth);
+}
+
+template <typename T>
+inline void BoundBox3<T>::setFromCenterAndXYZ(const Vector3& center, T sizeX, T sizeY, T sizeZ)
+{
+    setFromCenterAndXYZ(center.x, center.y, center.z, sizeX, sizeY, sizeZ);
+}
+
+template <typename T>
+Vector3<T> BoundBox3<T>::getCenter() const
+{
+    return Vector3((mMin.x + mMax.x) / 2.0f, (mMin.y + mMax.y) / 2.0f, (mMin.z + mMax.z) / 2.0f);
+}
+
+template <typename T>
+void BoundBox3<T>::getCenter(Vector3* p) const
+{
+    p->set((mMin.x + mMax.x) / 2.0f, (mMin.y + mMax.y) / 2.0f, (mMin.z + mMax.z) / 2.0f);
+}
+
+template <typename T>
+bool BoundBox3<T>::isInside(const Vector3& p) const
+{
+    return mMin.x <= p.x && p.x <= mMax.x &&
+           mMin.y <= p.y && p.y <= mMax.y &&
+           mMin.z <= p.z && p.z <= mMax.z;
+}
+
+template <typename T>
+inline void BoundBox3<T>::offset(T dx, T dy, T dz)
+{
+    mMin.x += dx;
+    mMin.y += dy;
+    mMin.z += dz;
+    mMax.x += dx;
+    mMax.y += dy;
+    mMax.z += dz;
+}
+
+template <typename T>
+inline void BoundBox3<T>::offset(const Vector3& dv)
+{
+    offset(dv.x, dv.y, dv.z);
+}
+
+template <typename T>
+inline void BoundBox3<T>::scaleX(T sx)
+{
+    T hx = getSizeX() * (sx / 2.0f);
+    T ox = (mMin.x + mMax.x) / 2.0f;
+
+    mMin.x = ox - hx;
+    mMax.x = ox + hx;
+}
+
+template <typename T>
+inline void BoundBox3<T>::scaleY(T sy)
+{
+    T hy = getSizeY() * (sy / 2.0f);
+    T oy = (mMin.y + mMax.y) / 2.0f;
+
+    mMin.y = oy - hy;
+    mMax.y = oy + hy;
+}
+
+template <typename T>
+inline void BoundBox3<T>::scaleZ(T sz)
+{
+    T hz = getSizeZ() * (sz / 2.0f);
+    T oz = (mMin.z + mMax.z) / 2.0f;
+
+    mMin.z = oz - hz;
+    mMax.z = oz + hz;
+}
 
 } // namespace sead
